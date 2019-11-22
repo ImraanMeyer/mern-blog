4f775c18da0f3fbfce49c5d18d51894ef16e25b9
ImraanMeyer <imraan.meyer97@gmail.com>
2019-11-22 19:04:40 +0200

    CRA

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <div>
+        <h1>Hey</h1>
+      </div>
+
+      <style jsx>{`
+        div {
+          background: red;
+        }
+      `}</style>
+    </React.Fragment>
+  )
+}
+
+export default App;
